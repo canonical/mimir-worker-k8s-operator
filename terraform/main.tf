57e2e9fce47d2f1c35d4c3d00b11878e3eedb306
Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
2024-10-07 15:10:30 -0300

    variable trust removed

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "juju_application" "mimir_worker" {
   name = var.app_name
   # Coordinator and worker must be in the same model
   model = var.model_name
-  trust = var.trust
+  trust = true  # We always need this variable to be true in order to be able to apply resources limits. 
 
   charm {
     name     = "mimir-worker-k8s"
