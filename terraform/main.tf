00936bf20a3a6e175e06b1796d706a69e2513fc6
Leon <82407168+sed-i@users.noreply.github.com>
2024-10-18 15:07:07 -0400

    Add terraform module (#74)
    * Add module draft
    
    * WIP
    
    * Fixes
    
    * Print diff in tff
    
    * Add model before tft
    
    * fmt
    
    * Add larger deployment
    
    * Fix
    
    * fixes
    
    * formatting
    
    * add comment in README
    
    * update juju provider version
    
    * fix README
    
    * remove terrafor tests (for now)
    
    * add constraints variable
    
    * variables in alphabetical order
    
    * add empty string as default value
    
    * use CI version of terraform-checks
    
    * variable trust removed
    
    * README updated
    
    * linting
    
    * version fix
    
    ---------
    
    Co-authored-by: Jose C. Massón <jose.masson@canonical.com>
    Co-authored-by: Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+resource "juju_application" "mimir_worker" {
+  name = var.app_name
+  # Coordinator and worker must be in the same model
+  model = var.model_name
+  trust = true # We always need this variable to be true in order to be able to apply resources limits. 
+
+  charm {
+    name     = "mimir-worker-k8s"
+    channel  = var.channel
+    revision = var.revision
+  }
+  units  = var.units
+  config = var.config
+}
