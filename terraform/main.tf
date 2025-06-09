249bedadba9d7900ec0c7ff49f8d8d41dbbd5ee6
Michael Thamm <mike.thamm@canonical.com>
2025-06-09 14:44:33 -0400

    feat: TF housekeeping part 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,15 @@
 resource "juju_application" "mimir_worker" {
-  name = var.app_name
-  # Coordinator and worker must be in the same model
-  model = var.model_name
-  trust = true # We always need this variable to be true in order to be able to apply resources limits. 
+  name               = var.app_name
+  config             = var.config
+  constraints        = var.constraints
+  model              = var.model
+  storage_directives = var.storage_directives
+  trust              = true
+  units              = var.units
 
   charm {
     name     = "mimir-worker-k8s"
     channel  = var.channel
     revision = var.revision
   }
-  units  = var.units
-  config = var.config
 }
