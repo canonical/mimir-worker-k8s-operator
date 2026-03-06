ab8c5ef74e770c70633237607f1f932f43cdb299
Michael Thamm <mike.thamm@canonical.com>
2026-03-06 15:11:52 -0500

    fix: Split TF endpoints output to requires/provides

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,13 @@ output "app_name" {
   value = juju_application.mimir_worker.name
 }
 
-output "endpoints" {
+output "provides" {
   value = {
-    # Requires
+  }
+}
+
+output "requires" {
+  value = {
     mimir_cluster = "mimir-cluster"
-    # Provides
   }
 }
